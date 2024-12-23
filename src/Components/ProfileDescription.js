57192d0c4dfa08c95dc7ad865f03e965e4c57a86
unknown <yossrabenmansour53@gmail.com>
2024-12-23 18:50:31 +0200

    ajout deux paragraphe dans la description

--- src/Components/ProfileDescription.js
+++ src/Components/ProfileDescription.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const ProfileDescription = () => {
-    return <p>Développeuse Web avec 4 ans d'expérience.</p>;
-};
+    return 
+    (
+   <div>
+     <h1>Développeuse Web avec 4 ans d'expérience.</h1>
+     <p>1ere expérience:</p>
+     <p>2emme experience :</p>
+     <p>3emme expérience :</p>
+   </div>
+
+)};
 
 export default ProfileDescription;
